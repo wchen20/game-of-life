440213fecd557f2ecb280c1034dc06e81d0f534d
jenkins <jenkins@centos69.(none)>
2018-01-01 19:22:33 +0800

    Changes by chenwei

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.wakaleo.gameoflife.domain.Universe;
 
  /**
+ * Modified by Chenwei
  * Wakaleo Consulting - John Ferguson Smart
  * Game of life, demonstration application for Jenkins: The Definitive Guide
  *
